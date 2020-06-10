39b4bf6c4502707e00a0e52f0a15429e7583bd61
Hend ElGhazaly <helghazaly@aus.edu>
2020-06-10 14:10:45 +0400

    testing

--- Calculations.java
+++ Calculations.java
@@ -1,4 +1,3 @@
-
 public class Calculations {
 
 	public static void main(String[] args) {
@@ -16,19 +15,6 @@ public class Calculations {
 		//member_D add the feature of multiplying x by y and print the result
 		
 		
-		
-		//Exercise 5: Spot the issues to be reported
-		addition(4,5); // should print the sum of the values provided
-		welcome(); // should print the welcome message to the console
-	}
-	
-	public static int addition(int w, int z) {
-		int sum = w - z;
-		return sum; 
-	}
-	
-	public static String welcome() {
-		return "Hello World"; 
 	}
 
 }
---
+++ CalculationsSoln.java
@@ -0,0 +1,28 @@
+
+public class CalculationsSoln {
+
+	public static void main(String[] args) {
+		int x = 2;
+		int y = 1;
+		
+		//sum of x and y 
+		int sum = x+y;
+		System.out.println("The sum is= "+ sum);
+		
+		//member_B subtract x from y and print the result
+		int subtraction = x-y;
+		System.out.println("The difference is ="+ subtraction);
+
+		//member_C divide x by y and print the result
+		int division = x/y;
+		System.out.println("The quotient is= "+ division);
+		
+		//member_D multiply x by y and print the result
+		int multiplication = x*y;
+		System.out.println("The product is = "+ multiplication);
+		
+
+	}
+	
+
+}
